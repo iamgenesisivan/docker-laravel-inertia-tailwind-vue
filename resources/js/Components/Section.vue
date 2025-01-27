4d43c8932ba1cd72275eee66fef4614fa05ce341
MONSTEEEER <aquino.igenesis@gmail.com>
2025-01-27 17:06:15 +0800

    First commit - done upto section 3
    to be follow

---
+++ resources/js/Components/Section.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="
+    px-72
+    py-10
+    ">
+        <slot>
+
+        </slot>
+    </section>
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,185 @@
+<template>
+    <div>
+        <Head title="Welcome" />
+
+        <Section class="
+            grid
+            grid-cols-2
+            text-right
+            bg-gray-800
+            text-gray-300
+        ">
+            <div>
+                <jet-application-mark class="h-12 w-auto"></jet-application-mark>
+            </div>
+            <div v-if="canLogin">
+                <Link v-if="$page.props.user" :href="route('dashboard')" class="text-sm underline">
+                    Dashboard
+                </Link>
+
+                <template v-else>
+                    <Link :href="route('login')" class="text-base underline">
+                        Log in
+                    </Link>
+
+                    <Link v-if="canRegister" :href="route('register')" class="text-base underline place-self-end ml-4">
+                        Register
+                    </Link>
+                </template>
+            </div>
+        </Section>
+
+        <Section 
+            class="
+                bg-gray-800
+                pt-16
+                h-screen
+            "
+        >
+            <div
+                class="h-2/3 flex flex-wrap content-between pb-36"
+            >
+                <p
+                    class="
+                        border-b-2
+                        font-bold
+                        border-gray-400
+                        pb-3
+                        text-2xl
+                        text-gray-300
+                        uppercase
+    
+                    "
+                >
+                Hey! This is Genesis. I'm a software engineer and I would be glad to work with you.</p>
+                <div class="flex items-end border-b-2 border-b-gray-500 pb-2">
+                    <p
+                        class="
+                            font-bold
+                            mr-5
+                            text-gray-500
+                            text-xl
+                        "
+                    >Want to know more?</p>
+                    <jet-button
+                        class="
+                            bg-green-400
+                            font-bold
+                            rounded
+                            text-sm
+                            text-gray-800
+                            hover:bg-green-800
+                        "
+                    >
+                        Let's chat
+                    </jet-button>
+                </div>
+            </div>
+
+            <div class="
+                animate-pulse mt-3 text-gray-300 text-center text-9xl
+            ">
+                <a href="#skills">&#8675;</a>
+            </div>
+        </Section>
+        
+        <Section id="skills" class="bg-gray-200 text-gray-800 h-screen">
+            <h2
+                class="
+                    text-6xl
+                    font-bold
+                    pt-3
+
+                "
+            >Skills</h2>
+            
+            <div class="flex justify-center mt-10">
+                <jet-button
+                    class="
+                        bg-indigo-800
+                        font-bold
+                        rounded
+                        text-sm
+                        text-gray-200
+                        hover:bg-indigo-700
+                    "
+                >
+                    Get in touch
+                </jet-button>
+            </div>
+
+        </Section>
+        
+        <Section class="bg-gray-600 text-gray-200 h-screen">
+            <h2
+            
+            class="
+            text-6xl
+            font-bold
+            pt-3
+           "
+            >Projects</h2>
+
+            <div class="flex justify-center mt-10">
+                <jet-button
+                    class="
+                        bg-purple-100
+                        font-bold
+                        rounded
+                        text-sm
+                        text-gray-800
+                        hover:bg-purple-700
+                    "
+                >
+                    Know more
+                </jet-button>
+            </div>
+
+        </Section>
+        
+        <Section
+            class="
+                flex
+                justify-between
+                bg-gray-800
+                text-gray-300
+                text-xl
+            "
+        >
+            <p>&copy; iGenesis. All rights reserved.</p>
+            <div
+                class="
+                    flex justify-evenly items-center
+                "
+            >
+                Github
+                Twitter
+                StackOverflow
+            </div>
+        </Section>
+    </div>
+</template>
+<script>
+
+import { Head, Link } from '@inertiajs/vue3';
+import { defineComponent } from 'vue';
+import JetApplicationMark from '../Components/ApplicationMark.vue'
+import JetButton from '../Components/PrimaryButton.vue'
+import Section from '../Components/Section.vue'
+
+    export default defineComponent({
+        components: {
+            Head,
+            Link,
+            JetApplicationMark,
+            Section,
+            JetButton,
+        },
+        props: {
+            canLogin: Boolean,
+            canRegister: Boolean,
+        }
+    })
+</script>
+
+
